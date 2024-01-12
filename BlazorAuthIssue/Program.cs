805fe180156f82d2df7cf699a5a2127bbaf1a347
noak-visma <noak.rosengren@visma.com>
2024-01-12 11:39:26 +0100

    Issue situation

--- BlazorAuthIssue/Program.cs
+++ BlazorAuthIssue/Program.cs
@@ -1,6 +1,8 @@
 using BlazorAuthIssue.Components;
 using BlazorAuthIssue.Components.Account;
 using BlazorAuthIssue.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -21,6 +23,7 @@ namespace BlazorAuthIssue
             builder.Services.AddScoped<IdentityUserAccessor>();
             builder.Services.AddScoped<IdentityRedirectManager>();
             builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+            builder.Services.AddScoped<IAuthorizationHandler, RequirementHandler>();
 
             builder.Services.AddAuthentication(options =>
                 {
@@ -28,6 +31,16 @@ namespace BlazorAuthIssue
                     options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
                 })
                 .AddIdentityCookies();
+
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("GroupPolicy", policy =>
+                {
+                    policy.Requirements.Add(new Requirement()); // This is added to check Authorization
+
+                });
+
+            });
 
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -69,4 +82,43 @@ namespace BlazorAuthIssue
             app.Run();
         }
     }
+
+    public class RequirementHandler : AuthorizationHandler<Requirement>
+    {
+        private readonly NavigationManager _navigationManager;
+        private readonly AuthenticationStateProvider _authenticationStateProvider;
+
+        public RequirementHandler(NavigationManager navigationManager, AuthenticationStateProvider authenticationStateProvider)
+        {
+            _navigationManager = navigationManager;
+            _authenticationStateProvider = authenticationStateProvider;
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, Requirement requirement)
+        {
+            var groupId = ExtractIdFromUrl();
+            var user = context.User;
+
+            if (!string.IsNullOrEmpty(groupId) && user?.HasClaim("GroupId", groupId) == true)
+            {
+                context.Succeed(requirement);
+            }
+
+            await Task.CompletedTask;
+        }
+
+        private string? ExtractIdFromUrl()
+        {
+            var uri = _navigationManager.ToAbsoluteUri(_navigationManager.Uri);
+            var segments = uri.Segments;
+
+            if (segments.Length < 4)
+                return null;
+
+            var Id = segments[3].TrimEnd('/');
+            return Id;
+        }
+    }
+
+    public class Requirement : IAuthorizationRequirement { }
 }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # BlazorAuthIssue
+
+Changes from default Scaffold (authorization with individual accounts)
+
+Register.razor - Adds the claim for my-group for every registered user
+
+Counter.razor - URL slightly changed to include the groupId and Authorization check
+
+App.Razor - Sets renderMode for all pages that are not Identity pages to InteractiveServer
+
+Program.cs - Added RequirementHandler and a GroupPolicy - This is where things goes wrong.
